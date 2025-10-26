943d5554258aea98c1a6fcc33a5f3b8f55ae6c9e
sanchyopas <saniagolovanev@gmail.com>
2025-10-26 17:25:30 +0700

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
 import * as functions from './modules/baseFunctions.js';
 
 functions.isWebp();
+
+import Swiper from "swiper";
+import {Navigation, Pagination, Scrollbar } from "swiper/modules";
+
+const heroSlider = new Swiper('.hero__slider', {
+  modules: [Navigation, Scrollbar],
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  navigation: {
+    nextEl: '.hero__arrow-next',
+    prevEl: '.hero__arrow-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
