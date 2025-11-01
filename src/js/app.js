f13da4b154dc81ddae96f1ad74bbb2098532fb46
avroratomsk <avroratomsk70@yandex.ru>
2025-11-01 14:11:39 +0300

    dev page single

--- src/js/app.js
+++ src/js/app.js
@@ -3,36 +3,105 @@ import * as functions from './modules/baseFunctions.js';
 functions.isWebp();
 
 import Swiper from "swiper";
-import {Navigation, Pagination, Scrollbar } from "swiper/modules";
+import {Navigation, Pagination, Scrollbar, Thumbs} from "swiper/modules";
 
 const heroSlider = new Swiper('.hero__slider', {
-  modules: [Navigation, Scrollbar],
-  direction: 'horizontal',
-  slidesPerView: 1,
-  spaceBetween: 20,
+    modules: [Navigation, Scrollbar],
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 20,
 
-  navigation: {
-    nextEl: '.hero__arrow-next',
-    prevEl: '.hero__arrow-prev',
-  },
+    navigation: {
+        nextEl: '.hero__arrow-next',
+        prevEl: '.hero__arrow-prev',
+    },
 
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
 
 const clientsSlider = new Swiper('.clients__slider', {
-  modules: [Navigation, Scrollbar],
-  direction: 'horizontal',
-  slidesPerView: 4.5,
-  spaceBetween: 20,
+    modules: [Navigation, Scrollbar],
+    direction: 'horizontal',
+    slidesPerView: 4.5,
+    spaceBetween: 20,
 
-  navigation: {
-    nextEl: '',
-    prevEl: '',
-  },
+    navigation: {
+        nextEl: '',
+        prevEl: '',
+    },
 
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+const infoSliderThumb = new Swiper('.info__slider-thumb', {
+
+    direction: 'vertical',
+    loop: false,
+    autoHeight: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    //   draggable: true,
+    // },
+
+    // breakpoints: {
+    //   320: {
+    //     scrollbar: {
+    //       enabled: true
+    //     },
+    //     slidesPerView: 1,
+    //   },
+    //   992: {
+    //     slidesPerView: 3,
+    //     scrollbar: {
+    //       enabled: false
+    //     },
+    //   }
+    // }
+
+});
+
+const infoSlider = new Swiper('.info__slider', {
+    modules: {Scrollbar, Pagination, Thumbs},
+    direction: 'horizontal',
+    loop: false,
+    autoHeight: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+
+    pagination: {
+        nextEl: '.project__slider-next',
+        prevEl: '.project__slider-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+
+    thumbs: {
+        swiper: infoSliderThumb,
+    },
+
+    // breakpoints: {
+    //   320: {
+    //     scrollbar: {
+    //       enabled: true
+    //     },
+    //     slidesPerView: 1,
+    //   },
+    //   992: {
+    //     slidesPerView: 3,
+    //     scrollbar: {
+    //       enabled: false
+    //     },
+    //   }
+    // }
+
 });
